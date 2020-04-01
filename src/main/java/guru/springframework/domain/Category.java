2eb9844d38c091f4454ade6e896ca67e84f4c72b
Alexandru Miclaus <extern.alexandru.miclaus@volkswagen.de>
2020-04-01 21:10:31 +0300

    Relation OneToOne, ManyToOne, ManyToMany + ENUM

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,31 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
